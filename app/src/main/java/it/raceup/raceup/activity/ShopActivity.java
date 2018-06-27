bdffa9994658e6557fc2e7ae388865d6212c6eca
stefano <sirfoga@protonmail.com>
2018-06-27 17:21:07 +0200

    add empty shopping activity

--- app/src/main/java/it/raceup/raceup/activity/ShopActivity.java
+++ app/src/main/java/it/raceup/raceup/activity/ShopActivity.java
@@ -1,7 +1,10 @@
 package it.raceup.raceup.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 import it.raceup.raceup.R;
 
@@ -11,5 +14,25 @@ public class ShopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+        setup();
+    }
+
+    private void setup() {
+        setupCardListeners();
+    }
+
+    private void setupCardListeners() {
+        CardView card = findViewById(R.id.aero);
+        card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openShopDetailActivity();
+            }
+        });
+    }
+
+    protected void openCheckoutActivity() {
+        Intent openActivity = new Intent(CommunityActivity.this, ForumDetailActivity.class);
+        startActivity(openActivity);
     }
 }
