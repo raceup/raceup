ca185dcbece682c83a09047763ff50e31bedc721
stefano <sirfoga@protonmail.com>
2018-06-27 22:15:38 +0200

    fix checkout and payment activity

--- app/src/main/java/it/raceup/raceup/activity/CheckoutShopActivity.java
+++ app/src/main/java/it/raceup/raceup/activity/CheckoutShopActivity.java
@@ -1,8 +1,12 @@
 package it.raceup.raceup.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import it.raceup.raceup.R;
 
@@ -14,6 +18,22 @@ public class CheckoutShopActivity extends AppCompatActivity {
         setContentView(R.layout.activity_checkout_shop);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setup();
+    }
+
+    private void setup() {
+        Button button = findViewById(R.id.pay_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCheckoutActivity();
+            }
+        });
+    }
+
+    protected void openCheckoutActivity() {
+        Intent openActivity = new Intent(CheckoutShopActivity.this, PaymentShopActivity.class);
+        startActivity(openActivity);
     }
 
 }
--- app/src/main/java/it/raceup/raceup/activity/ShopActivity.java
+++ app/src/main/java/it/raceup/raceup/activity/ShopActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Button;
 
 import it.raceup.raceup.R;
 
@@ -33,6 +34,14 @@ public class ShopActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent openActivity = new Intent(ShopActivity.this, WheelShopActivity.class);
                 startActivity(openActivity);
+            }
+        });
+
+        Button button = findViewById(R.id.checkout_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCheckoutActivity();
             }
         });
     }
