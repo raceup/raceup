495dde34f216b720f6d0f87eb1f32b91c60bf568
stefano <sirfoga@protonmail.com>
2018-06-27 21:52:11 +0200

    add wheels shop

--- app/src/main/java/it/raceup/raceup/activity/WheelShopActivity.java
+++ app/src/main/java/it/raceup/raceup/activity/WheelShopActivity.java
@@ -3,6 +3,9 @@ package it.raceup.raceup.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import it.raceup.raceup.R;
 
@@ -14,6 +17,45 @@ public class WheelShopActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wheel_shop);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        setup();
     }
 
+    private void setup() {
+        ImageButton button = findViewById(R.id.button_braid);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Added x4 BRAID wheels");
+            }
+        });
+
+        button = findViewById(R.id.button_blackwave);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Added x4 BLACKWAVE wheels");
+            }
+        });
+
+        button = findViewById(R.id.button_oz);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Added x4 OZ wheels");
+            }
+        });
+
+        button = findViewById(R.id.button_avon);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("Added x4 AVON wheels");
+            }
+        });
+    }
+
+    protected void showToast(String message) {
+        Toast.makeText(WheelShopActivity.this, message, Toast.LENGTH_LONG).show();
+    }
 }
