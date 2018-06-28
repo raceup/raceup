af85b3fd796b094309684964449d600b86417785
stefano <sirfoga@protonmail.com>
2018-06-28 16:41:43 +0200

    add checkout cart

--- app/src/main/java/it/raceup/raceup/activity/PaymentShopActivity.java
+++ app/src/main/java/it/raceup/raceup/activity/PaymentShopActivity.java
@@ -3,6 +3,9 @@ package it.raceup.raceup.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import it.raceup.raceup.R;
 
@@ -14,6 +17,21 @@ public class PaymentShopActivity extends AppCompatActivity {
         setContentView(R.layout.activity_payment_shop);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setup();
+    }
+
+    private void setup() {
+        Button button = findViewById(R.id.pay_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("All done! Payment successful!");
+            }
+        });
+    }
+
+    protected void showToast(String message) {
+        Toast.makeText(PaymentShopActivity.this, message, Toast.LENGTH_LONG).show();
     }
 
 }
